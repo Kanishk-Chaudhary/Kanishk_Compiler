f654ad88e7ff2fd1e6315ac6d2640d2d86d93284
Kanishk Chaudhary <127675570+Kanix7@users.noreply.github.com>
2024-06-14 09:23:26 +0530

    changes comments

--- src/main.cpp
+++ src/main.cpp
@@ -6,37 +6,36 @@
 
 #include "tokenization.h"
 
-
-std::string tokens_to_asm(const std::vector<Token>& tokens)
+std::string tokens_to_asm(const std::vector<Token> &tokens)
 {
-    std::stringstream output; // Changed to stringstream
-    output << "global _start\n_start:\n"; // Changed "_start" to "_start"
+    std::stringstream output;
+    output << "global _start\n_start:\n";
 
-    for (size_t i = 0; i < tokens.size(); i++) // Changed int to size_t
+    for (size_t i = 0; i < tokens.size(); i++)
     {
-        const Token& token = tokens.at(i); // Removed extra 'n' in token
+        const Token &token = tokens.at(i);
         if (token.type == TokenType::exit)
         {
             if (i + 2 < tokens.size() && tokens.at(i + 1).type == TokenType::int_lit &&
-                tokens.at(i + 2).type == TokenType::semi) // Corrected the conditions
+                tokens.at(i + 2).type == TokenType::semi)
             {
                 output << " mov rax, 60\n";
                 output << " mov rdi, " << tokens.at(i + 1).value.value_or("0") << "\n";
 
-                output << " syscall\n"; // Added missing '\n' at the end
-                i += 2; // Incremented 'i' by 2 to skip processed tokens
+                output << " syscall\n";
+                i += 2;
             }
         }
     }
     return output.str();
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        std::cerr << "Incorrect usage. Correct usage is..." <<std::endl;
-        std::cerr << "Kanishk_Compiler <input.kc>" <<std::endl;
+        std::cerr << "Incorrect usage. Correct usage is..." << std::endl;
+        std::cerr << "Kanishk_Compiler <input.kc>" << std::endl;
         return EXIT_FAILURE;
     }
 
@@ -53,13 +52,12 @@ int main(int argc, char* argv[])
     std::cout << tokens_to_asm(tokens) << std::endl;
 
     {
-        std::fstream file("out.asm",std::ios::out);
+        std::fstream file("out.asm", std::ios::out);
         file << tokens_to_asm(tokens);
     }
 
     system("nasm -felf64 out.asm");
     system("ld -o out out.o");
-
 
     return EXIT_SUCCESS;
 }
